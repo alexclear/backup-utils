9587e58498e0920df0af21bcd7d2a207d0ca3146
Alex Chistyakov <alexclear@gmail.com>
2011-11-01 10:31:28 +0400

    Removing debug output, fixing a problem with "01" to "1" string comparison

--- zfssnapshotscleaner.rb
+++ zfssnapshotscleaner.rb
@@ -2,11 +2,12 @@ begin
   `zfs list -t snapshot`.split(/\n/).drop(1).each do |snapshot|
     name = snapshot.split(/ /)[0]
     year, mon, day, hour = name.split(/@/)[1].split(/-/)
-    puts "#{year} #{mon} #{day} #{hour}"
-    if (day != Time.new.day.to_s && day.to_i != (Time.new.day.to_i - 1)) && (hour != '00')
-      puts Time.new.day
-      puts "Deleting #{year} #{mon} #{day} #{hour}"
+#    puts "#{year} #{mon} #{day} #{hour}"
+    if (day.to_i != Time.new.day.to_i && day.to_i != (Time.new.day.to_i - 1)) && (hour != '00')
+#      puts Time.new.day
+#      puts "Deleting #{year} #{mon} #{day} #{hour}"
       %x[zfs destroy #{name}]
+      sleep(5)
     end
   end
 end
